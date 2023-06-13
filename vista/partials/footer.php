9bae17e5522b2db256699c0504f763ece4e713ae
Shait2 <saitaleman69@gmail.com>
2023-06-12 23:24:03 -0600

    update

--- vista/partials/footer.php
+++ vista/partials/footer.php
@@ -1,10 +1,10 @@
-    <!-- Bootstrap core JavaScript
-    ================================================== -->
-    <!-- Placed at the end of the document so the pages load faster -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
-    <script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jqueryui/1.10.4/jquery-ui.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
-	<script type="text/javascript" src="assets/js/overhang.min.js"></script>
-    <!--<script src="assets/js/app.js"></script>-->
-  </body>
+<!-- Bootstrap core JavaScript
+================================================== -->
+<!-- Placed at the end of the document so the pages load faster -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js" integrity="sha384-2zjw1So+sKeG+QDzB+KmCkTNXKAJo/uszkD4UJd5yWg2i/z8KmDf4Z2D4/gG/xkk" crossorigin="anonymous"></script>
+<script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jqueryui/1.10.4/jquery-ui.min.js" integrity="sha384-1Y1yIV/OkKn8tcxjbDBbBgq1iuItWR2e5p1dxPfl/wylCSWgG3cJJ2dz5sBB1yk+" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-2C8G4GpKrt2zAln3+2qRj9B+D1ph/Z6n4PA4JYIoD2//5b1JdssMH/ooDg6Q5M/9" crossorigin="anonymous"></script>
+<script type="text/javascript" src="assets/js/overhang.min.js"></script>
+<!--<script src="assets/js/app.js"></script>-->
+</body>
 </html>
